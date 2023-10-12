93a297f7d99d3687f88b5c8b20dd02e5d256071f
mate.cserhati <mate.cserhati>
2023-10-12 19:55:11 +0200

    GUI is finished

---
+++ playfairGUI/FrmMain.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace playfairGUI
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rTxtBText = new System.Windows.Forms.RichTextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rTxtBText
+            // 
+            this.rTxtBText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rTxtBText.Location = new System.Drawing.Point(12, 32);
+            this.rTxtBText.Name = "rTxtBText";
+            this.rTxtBText.Size = new System.Drawing.Size(776, 406);
+            this.rTxtBText.TabIndex = 0;
+            this.rTxtBText.Text = "HI DE TH EG OL DI NT HE TR EX ES TU MP";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Green;
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(150, 20);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Előkészített szöveg:";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.rTxtBText);
+            this.Name = "FrmMain";
+            this.Text = "playfairGUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rTxtBText;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ playfairGUI/FrmMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace playfairGUI
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            rTxtBText.TextChanged += RTxtBText_TextChanged;
+        }
+
+        private void RTxtBText_TextChanged(object sender, EventArgs e)
+        {
+            string[] ell = rTxtBText.Text.Split(' ');
+            foreach (var item in ell)
+            {
+                if (item.Length > 2 || item.Length < 2)
+                {
+                    lblTitle.ForeColor = Color.Red;
+                }
+                else if ((item[0] < 64 || item[0] > 91 || (item[1] < 64 || item[1] > 91)) || char.IsLower(item[0]) || char.IsLower(item[1]))
+                {
+                    lblTitle.ForeColor = Color.Blue;
+                }
+                else if (item[0] == item[1])
+                {
+                    lblTitle.ForeColor = Color.Magenta;
+                }
+                else
+                {
+                    lblTitle.ForeColor = Color.Green;
+                }
+
+
+            }
+        }
+    }
+}
