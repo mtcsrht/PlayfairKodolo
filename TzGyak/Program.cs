2bb043212f1f4596247df868544e62d97d344312
mate.cserhati <mate.cserhati>
2023-10-12 19:09:33 +0200

    Add project files.

---
+++ TzGyak/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TzGyak
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            PlayfairKodolo playfair = new PlayfairKodolo("kulcstabla.txt");
+
+            Console.Write($"6. feladat - Kérek egy nagybetűt: ");
+            string resp = Console.ReadLine().ToUpper();
+            Console.WriteLine($"A karakter sorának indexe: {playfair.SorIndex(char.Parse(resp))}" +
+                $"\nA karakter oszlopának indexe: {playfair.OszlopIndex(char.Parse(resp))}");
+            Console.Write($"8. feladat - Kérek egy karakterpárt: ");
+            resp = Console.ReadLine().ToUpper();
+            Console.WriteLine($"Kódolva: {playfair.KodolBetupar(resp)}");
+            Console.ReadLine();
+        }
+    }
+}
